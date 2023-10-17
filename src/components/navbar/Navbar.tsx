60d4c3547b89877a0d0195be6ada3738bd20b168
Mikko <mikko.airikka@gmail.com>
2023-10-17 11:09:50 +0300

    Bring in brand elements

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,8 +3,16 @@ import styles from './Navbar.module.css'
 
 const Navbar: React.FC = () => {
     return (
-        <nav style={{marginBottom: '40px'}}>
+        <nav style={{marginBottom: '80px'}}>
             <div className={styles.navbar}>
+                <div className={styles.brandContent}>
+                    <div className={styles.logoArea}>
+                    </div>
+                    <div className={styles.nameArea}><span className={styles.whiteColor}>VISUALIZING</span><span className={styles.redColor}>NHL</span>
+                    </div>
+                    <div className={styles.sloganArea}> | NHL Stats on a Timeline
+                    </div>
+                </div>
                 <p className={styles.topRight}>VISUALIZINGNHL.COM</p>
             </div>
             <hr className={styles.hr} />
